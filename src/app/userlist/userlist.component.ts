27fc3e768a2c79ff9684d4bb70208efbb4cbe3e5
LuisAlegria27 <alegria.luis@hlsgroup.com.mx>
2023-10-12 11:03:16 -0600

    12/10/23

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from '../api/user-service.service';
-import { Route } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
